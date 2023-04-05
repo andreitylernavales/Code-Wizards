b63a48943d0ebc066b5cd58b14c53fa5a7d68b79
Joane Ondin <123534814+joaneondin@users.noreply.github.com>
2023-04-05 08:49:12 +0800

    Update My-UI.py

--- My-UI.py
+++ My-UI.py
@@ -7,11 +7,10 @@ class MyUI:
         self.master = master
         self.master.title("My_Calendar_UI_design")
         self.master.geometry("1000x1000")
-        self.master.configure(bg="#ecc19c")
+        self.master.configure(bg="black")
 
-        self.custom_font = font.Font(family="Times New Roman", size=40, weight="bold")
-
-        self.label = tk.Label(self.master, text="CODE_WIZARDS", font=self.custom_font, fg="#FFFF00", bg="#3b4d61")
+        self.label = tk.Label(self.master, text="CODE_WIZARDS", font=("Times New Roman", 40, "bold"), fg="#FFFF00",
+                              bg="#3b4d61")
         self.label.grid(row=0, column=6, padx=5, pady=5)
 
         self.year_label = tk.Label(self.master, text="Years:", fg="white", bg="#0000FF")
@@ -22,10 +21,15 @@ class MyUI:
         self.month_label.grid(row=2, column=0, padx=5, pady=5)
         self.month_entry = tk.Entry(self.master, bg="#40E0D0")
         self.month_entry.grid(row=2, column=1, padx=5, pady=5)
-        self.search_button = tk.Button(self.master, text="Show Calendar", fg="black", bg="#FF0000")
-        self.search_button.grid(row=2, column=3, padx=5, pady=5)
+        self.search_button = tk.Button(self.master, text="Show Calendar", fg="white",
+                                       bg="#FF0000")
+        self.search_button.grid(row=2, column=3, padx=10, pady=10)
+
+        self.letter = tk.Label(self.master, text="CALENDAR", font=("Times New Roman", 30), fg="black", bg="#FFD700")
+        self.letter.grid(row=2, column=5, columnspan=2, pady=5, padx=5)
 
 
 root = tk.Tk()
 MyUI = MyUI(root)
 root.mainloop()
+
