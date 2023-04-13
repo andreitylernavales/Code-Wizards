1033c439efbfa39e3ce46f8d6525e602d04e1bdf
Joane Ondin <123534814+joaneondin@users.noreply.github.com>
2023-04-13 21:53:56 +0800

    nah mag sorry ka

--- UI-design.py
+++ UI-design.py
@@ -1,38 +1,49 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import ttk
+
+gray_bg = '#5e5c5c'
+
+darkgray_bg = '#333333'
+
+
+def Switch_theme():
+    current_theme = root.cget('bg')
+    if current_theme == gray_bg:
+        root.config(bg=darkgray_bg)
+        for widget in root.winfo_children():
+            widget.config(bg=darkgray_bg)
+    else:
+        root.config(bg=gray_bg)
+        for widget in root.winfo_children():
+            widget.config(bg=gray_bg)
 
 
 class CalendarGUI:
     def __init__(self, parent):
         self.parent = parent
-        self.parent.title("My_Calendar_UI_design")
+        self.parent.title("My_Calendar_UI")
         self.parent.geometry("1200x800")
-        self.parent.configure(bg="#6E6E6E")
+        self.parent.configure(bg="#5e5c5c")
         self.month_entry = tk.Entry()
+        self.events = []
 
         self.current_month = datetime.date.today().month
         self.current_year = datetime.date.today().year
 
-        self.time_label = tk.Label(self.parent, font=("Times New Roman", 30), fg="white", bg="#333333")
-        self.time_label.grid(row=0, column=2, pady=15)
-
+        self.time_label = tk.Label(self.parent, font=("Times New Roman", 30, "bold"), fg="black", bg="#5e5c5c")
+        self.time_label.grid(row=0, column=1, pady=15)
         self.update_clock()
 
         self.label = tk.Label(self.parent, text="CODE_WIZARDS", font=("Times New Roman", 40, "bold"), fg="#FFFF00",
-                              bg="#6E6E6E")
+                              bg="#5e5c5c")
         self.label.grid(row=0, column=6, padx=5, pady=5)
 
-        self.year_label = tk.Label(self.parent, text="Years:", fg="black", bg="#6E6E6E", font=("Ariel", 10, "bold"))
-        self.year_label.grid(row=3, column=1, padx=10, pady=10)
-        self.year_entry = tk.Entry(self.parent, bg="#6E6E6E", font=("calibre", 15))
-        self.year_entry.grid(row=3, column=2, padx=10, pady=10)
-
-        self.search_button = tk.Button(self.parent, text="Show Calendar", command=self.create_calendar, fg="black",
-                                       bg="#6E6E6E", font=("Ariel", 10, "bold"))
-        self.search_button.grid(row=3, column=3, padx=10, pady=10)
+        self.year_label = tk.Label(self.parent, text="Search Years:", fg="black", bg="#5e5c5c", font=("Times New Roman", 15, "bold"))
+        self.year_label.grid(row=1, column=0, padx=10, pady=10)
+        self.year_entry = tk.Entry(self.parent, bg="#36454F", font=("calibre", 15, "bold"))
+        self.year_entry.grid(row=1, column=1, padx=10, pady=10)
 
 
 root = tk.Tk()
-MyUI = MyUI(root)
+MyUI = CalendarGUI(root)
 root.mainloop()
-
